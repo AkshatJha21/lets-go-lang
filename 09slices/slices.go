58689c39fd7e952e88eb58afca0910f49a19f0c8
AkshatJha21 <akshatj2134@gmail.com>
2026-07-18 23:38:16 +0530

    append slices

--- 09slices/slices.go
+++ 09slices/slices.go
@@ -12,8 +12,29 @@ func main() {
 	fmt.Println(nums == nil)
 	fmt.Println(len(nums))
 
-	var numsArr = make([]int, 2)
+	var numsArr = make([]int, 0, 5)
 	fmt.Println(numsArr)
 	fmt.Println(numsArr == nil)
+	// cap => capacity => maxm number of elements can fit
 	fmt.Println(cap(numsArr))
+	numsArr = append(numsArr, 1)
+	numsArr = append(numsArr, 2)
+	numsArr = append(numsArr, 3)
+	numsArr = append(numsArr, 4)
+	numsArr = append(numsArr, 5)
+	numsArr = append(numsArr, 6)
+	numsArr = append(numsArr, 7)
+	numsArr = append(numsArr, 8)
+	numsArr = append(numsArr, 9)
+	numsArr = append(numsArr, 10)
+	numsArr = append(numsArr, 11)
+	fmt.Println(numsArr)
+	fmt.Println(cap(numsArr))
+
+	numbers := []int{}
+	numbers = append(numbers, 1)
+	numbers = append(numbers, 2)
+	fmt.Println(numbers);
+	fmt.Println(cap(numbers));
+	fmt.Println(len(numbers));
 }
